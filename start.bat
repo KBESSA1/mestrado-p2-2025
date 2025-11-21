5dd4c5141d6adba25171f76729f3d46e2ee68c9e
Rodrigo Luiz Campos <rodrigoluizcampos@gmail.com>
2025-11-21 21:56:02 +0000

    Reset geral: espelha exatamente o estado do Docker (/workspace)

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Mapear a pasta com espaço para a letra M:
-subst M: "G:\Meu Drive\Mestrado 2025"
-
-REM Subir o container em segundo plano
-docker compose up -d
-
-REM Entrar no container interativo
-docker exec -it mestrado bash
--- stop.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Parar e remover o container
-docker compose down
-
-REM Desmontar o drive M:
-subst /D M:
